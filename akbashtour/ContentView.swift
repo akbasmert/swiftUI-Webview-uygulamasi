36fc85e88aa5af32015e6f5be765cba7983784e5
mert <mert.ma346@gmail.com>
2022-10-10 21:25:17 +0300

    webview eklendi, item bar site rengine göre ayarlandı, bottom da safearea iptal edildi.

--- akbashtour/ContentView.swift
+++ akbashtour/ContentView.swift
@@ -6,16 +6,51 @@
 //
 
 import SwiftUI
+import WebKit
+import UIKit
 
 struct ContentView: View {
+    let appearance = UINavigationBarAppearance()
+    
+    
+    @State private var showWebView = false
+       private let urlString: String = "https://www.akbashtour.com"
+       
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        GeometryReader{ geometry in
+            ZStack {
+                Color(red: 61 / 255, green: 47 / 255, blue: 230 / 255).edgesIgnoringSafeArea(.all) // İtem bar rengini kendi sitenize göre ayarlayabilirsiniz.
+                VStack() {
+                            // Normal WebView
+                            WebView(url: URL(string: urlString)!)
+                            
+                            // Create a link that opens in a new window
+                            Link(destination: URL(string: urlString)!, label: {
+                              
+                            })
+                            
+                            
+                        }
+            }.edgesIgnoringSafeArea(.bottom) // bottom da safearea iptal edildi.
         }
-        .padding()
+       
+    }
+}
+
+// WebView Struct
+struct WebView: UIViewRepresentable {
+    
+    var url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
     }
 }
 
