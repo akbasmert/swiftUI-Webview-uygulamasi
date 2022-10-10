590720b84b8a2f0e8c66171c9341697be2728bb5
mert <mert.ma346@gmail.com>
2022-10-10 19:58:49 +0300

    Initial Commit

---
+++ akbashtour/akbashtourApp.swift
@@ -0,0 +1,17 @@
+//
+//  akbashtourApp.swift
+//  akbashtour
+//
+//  Created by Mert AKBAŞ on 10.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct akbashtourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
